9e6e76a2271cd713c1d5f550a3fe497597718aa4
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-19 21:14:25 -0300

    + access-token

--- auth-flow/auth-code/src/index.ts
+++ auth-flow/auth-code/src/index.ts
@@ -14,6 +14,31 @@ app.get("/login", (req, res) => {
   res.redirect(url);
 });
 
+app.get("/callback", async (req, res) => {
+  console.log(req.query);
+
+  const bodyParams = new URLSearchParams({
+    client_id: "fs-client",
+    grant_type: "authorization_code",
+    code: req.query.code as string,
+    redirect_uri: "http://localhost:3000/callback",
+  });
+
+  const url = `http://host.docker.internal:8080/realms/fs-realm/protocol/openid-connect/token`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: bodyParams.toString(),
+  });
+
+  const result = await response.json();
+  console.log(result);
+  res.json(result);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
