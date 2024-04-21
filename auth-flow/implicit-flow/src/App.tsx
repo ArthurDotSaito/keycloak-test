3d30a8f3e7a618b4f6d1ecc337dac657dc93e56f
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-20 23:01:17 -0300

    basic pages

--- auth-flow/implicit-flow/src/App.tsx
+++ auth-flow/implicit-flow/src/App.tsx
@@ -1,3 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Admin } from "./Admin";
+import { Callback } from "./Callback";
+import { Login } from "./Login";
+import { Logout } from "./Logout";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "home",
+    element: <Logout />,
+  },
+  {
+    path: "admin",
+    element: <Admin></Admin>,
+  },
+  {
+    path: "callback",
+    element: <Callback />,
+  },
+]);
+
 function App() {
   return (
     <>
